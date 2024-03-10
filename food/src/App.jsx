ae2cf330fe367c581d3a9004ba633b2d38c9809a
vaqsi1990 <97800985+vaqsi1990@users.noreply.github.com>
2024-03-10 15:46:42 +0400

    Update App.jsx

--- food/src/App.jsx
+++ food/src/App.jsx
@@ -18,7 +18,7 @@ import Users from './Pages/Users'
 import SingleUser from './Pages/SingleUser';
 import AdminItems from './Pages/AdminItems';
 import About from './Pages/About';
-axios.defaults.baseURL = import.meta.env.VITE_URL
+axios.defaults.baseURL = "https://food-blog-vaqsi.vercel.app"
 
 axios.defaults.withCredentials = true
 function App() {
