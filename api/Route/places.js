58ba93aa19c82b6b35519f1786685fa070ba3ca3
vaqsi1990 <97800985+vaqsi1990@users.noreply.github.com>
2024-03-11 09:20:03 +0400

    Update places.js

--- api/Route/places.js
+++ api/Route/places.js
@@ -2,27 +2,27 @@ const express = require('express')
 const foodController = require('../Controller/food')
 const router = express.Router()
 
-router.get('/users-food', foodController.all)
+router.get('/api/users-food', foodController.all)
 
-router.get('/food/:id', foodController.edit)
+router.get('/api/food/:id', foodController.edit)
 
-router.put('/food', foodController.editFood)
+router.put('/api/food', foodController.editFood)
 
-router.delete('/delete/:id', foodController.deleteFood)
+router.delete('/api/delete/:id', foodController.deleteFood)
 
-router.get("/single/:id", foodController.singleFood)
-router.post('/comments/:foodId',   foodController.addComments)
+router.get("/api/single/:id", foodController.singleFood)
+router.post('/api/comments/:foodId',   foodController.addComments)
 
-router.get('/allFood', foodController.allFood)
+router.get('/api/allFood', foodController.allFood)
 
-router.delete('/comments/:commentId',  foodController.deleteComment);
+router.delete('/api/comments/:commentId',  foodController.deleteComment);
 
-router.post('/add-to-favourite', foodController.addToFavourite)
+router.post('/api/add-to-favourite', foodController.addToFavourite)
 
-router.delete('/remove-favourite/:favouriteId', foodController.removeFromFavourite)
+router.delete('/api/remove-favourite/:favouriteId', foodController.removeFromFavourite)
 
-router.get('/favourite', foodController.allFavourite)
+router.get('/api/favourite', foodController.allFavourite)
 
-router.get('/all-comments/:foodId', foodController.allComments)
+router.get('/api/all-comments/:foodId', foodController.allComments)
 
 module.exports = router;
